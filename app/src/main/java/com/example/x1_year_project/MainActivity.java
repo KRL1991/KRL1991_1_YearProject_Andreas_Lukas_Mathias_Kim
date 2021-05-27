c5c91e25dc057e79e8cb577956ad50d6af4bfac5
ravnen707 <ravnen707@gmail.com>
2021-05-27 14:55:59 +0200

    Created StartScreen with buttons to talk with catalog and other options.

--- app/src/main/java/com/example/x1_year_project/MainActivity.java
+++ app/src/main/java/com/example/x1_year_project/MainActivity.java
@@ -7,12 +7,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+    Button button = (Button) findViewById(R.id.btncatalog);
+    button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, Catalog.class);
+                    startActivity(intent);
+        }
+    });
+
+    Button button1 = (Button) findViewById(R.id.btninspiration);
+    button1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, Inspiration.class);
+            startActivity(intent);
+        }
+    });
 
     }
 }
