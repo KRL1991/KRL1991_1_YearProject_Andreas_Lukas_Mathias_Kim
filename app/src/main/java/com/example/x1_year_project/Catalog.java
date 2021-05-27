7e161b6b449a1293640e2ec6445df438f7400fe4
MMadsen99 <math9082@edu.dk>
2021-05-27 13:13:59 +0200

    Added pictures in Catalog and a scrollable view

---
+++ app/src/main/java/com/example/x1_year_project/Catalog.java
@@ -0,0 +1,34 @@
+package com.example.x1_year_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Catalog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
+
+
+
+
+
+ /*
+        setContentView(R.layout.activity_katalog);
+
+        ImageView førstebillede = findViewById(R.id.fag1med4glas);
+        førstebillede.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Mathias","Det fungere");
+            }
+        });
+        */
