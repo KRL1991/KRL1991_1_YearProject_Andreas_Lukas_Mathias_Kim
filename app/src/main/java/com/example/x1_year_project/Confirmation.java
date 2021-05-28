66ec4b22c5513aa6dc13f59738e61def5fb8e86b
KRL1991 <kimx4706@edu.easj.dk>
2021-05-28 08:51:48 +0200

    Added Confirmation activity and made it navigate to home screen with button press.

--- app/src/main/java/com/example/x1_year_project/Confirmation.java
+++ app/src/main/java/com/example/x1_year_project/Confirmation.java
@@ -2,7 +2,10 @@ package com.example.x1_year_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Confirmation extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Confirmation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
+
+        Button goHome = findViewById(R.id.ConfirmationButton);
+
+        goHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Confirmation.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
