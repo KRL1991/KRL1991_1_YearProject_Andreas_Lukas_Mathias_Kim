37580dc71449df0f241301c7f44e5f9b23462f71
lukas <lukasandersen_1@hotmail.com>
2021-05-31 11:55:14 +0200

    Første version Spinner

--- app/src/main/java/com/example/x1_year_project/InfoSite.java
+++ app/src/main/java/com/example/x1_year_project/InfoSite.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class InfoSite extends AppCompatActivity {
 
@@ -14,13 +16,20 @@ public class InfoSite extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_site);
 
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+            R.array.planets_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
 
 
     Button btn = (Button) findViewById(R.id.ChosenWallButton);
 
-        btn.setOnClickListener(new View.OnClickListener()
-
-    {
+        btn.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick (View v){
         Intent i = new Intent(InfoSite.this, Confirmation.class);
