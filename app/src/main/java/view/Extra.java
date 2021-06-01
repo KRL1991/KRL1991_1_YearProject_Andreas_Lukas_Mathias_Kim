f475cc8398eaf2a4dbba2319404e8ebb9e12e6e2
KRL1991 <kimx4706@edu.easj.dk>
2021-06-01 13:25:36 +0200

    Added startActivity(intent) to Extra and changed the Chosenwall intent to go to Extra Activity. Made a new bundle from ChosenWall in Extra. which now takes sets the data in ListOfChosenItems

--- app/src/main/java/view/Extra.java
+++ app/src/main/java/view/Extra.java
@@ -6,22 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.x1_year_project.R;
 
 public class Extra extends AppCompatActivity {
 
+
+// Mathias og Kim
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_extra);
 
-        Button goToBasket = findViewById(R.id.ExtraButton);
+        // A new bundle which gets the data from listData bundle.
+        Bundle listDataExtra = getIntent().getExtras();
+        String heightData = listDataExtra.getString("height");
+        String widthData = listDataExtra.getString("width");
+        String priceData = listDataExtra.getString("price");
 
+        Button goToBasket = findViewById(R.id.ExtraButton);
         goToBasket.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Extra.this,ListOfChosenItems.class);
+
+                // The intent combines the Strings and the keys.
+                intent.putExtra("height",heightData);
+                intent.putExtra("width", widthData);
+                intent.putExtra("price", priceData);
+
                 startActivity(intent);
             }
         });
