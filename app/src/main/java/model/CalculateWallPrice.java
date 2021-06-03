47a6628afb59866694a1a0e5eae68333de1d9c9f
KRL1991 <kimx4706@edu.easj.dk>
2021-06-03 11:30:23 +0200

    Added calculation to CalculateWallPrice

--- app/src/main/java/model/CalculateWallPrice.java
+++ app/src/main/java/model/CalculateWallPrice.java
@@ -2,5 +2,22 @@ package model;
 
 public class CalculateWallPrice {
 
+    double wallHeigth;
+    double wallWidth;
     int standardGlassHeight = 60;
+    int standardGlassWidth = 45;
+    int delivery = 800;
+
+
+    public CalculateWallPrice(double wallHeigth, double wallWidth, int standardGlassHeight, int standardGlassWidth, int delivery) {
+        this.wallHeigth = wallHeigth;
+        this.wallWidth = wallWidth;
+        this.standardGlassHeight = standardGlassHeight;
+        this.standardGlassWidth = standardGlassWidth;
+        this.delivery = delivery;
+
+        int glassSize = (int) ((wallHeigth/standardGlassHeight) * (wallWidth/standardGlassWidth) + delivery);
+    }
 }
+
+
