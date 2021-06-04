f80395935b781e799a4840012aa247bdfee3c76c
KRL1991 <kimx4706@edu.easj.dk>
2021-06-04 10:45:31 +0200

    Added clear function to the Basket array on home press

--- app/src/main/java/view/Confirmation.java
+++ app/src/main/java/view/Confirmation.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.x1_year_project.R;
+
+import model.Basket;
 
 public class Confirmation extends AppCompatActivity {
 
@@ -18,11 +21,16 @@ public class Confirmation extends AppCompatActivity {
 
         Button goHome = findViewById(R.id.ConfirmationButton);
 
+
         goHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Confirmation.this, StartScreen.class);
                 startActivity(intent);
+
+                Basket.getContent().clear();
+
+
             }
         });
     }
